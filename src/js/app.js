71ae417f0ba336d8004116161d30055b4cf5ce12
Damian Escobar <damianescobar2001@gmail.com>
2024-12-08 20:14:57 -0300

    todo el tema de dark-mode

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
 
     eventListener();
+
+    darkMode();
 });
 
 function eventListener() {
@@ -10,5 +12,38 @@ function eventListener() {
 }
 
 function navMobile() {
-    console.log('Diste click');
+    const navegacion = document.querySelector('.navegacion');
+
+    // Esta linea de codigo hace los mismo que el if de abajo
+    navegacion.classList.toggle('mostrar');
+
+    /*if (navegacion.classList.contains('mostrar')) {
+        navegacion.classList.remove('mostrar');
+    } else {
+        navegacion.classList.add('mostrar');
+    }*/
+}
+
+function darkMode() {
+    const prefiereDark = window.matchMedia('(prefers-color-scheme: dark)');
+    //console.log(prefiereDark.matches)
+
+    if (prefiereDark.matches) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+
+    prefiereDark.addEventListener('change', () => {
+        if (prefiereDark.matches) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    })
+
+    const bntDark = document.querySelector('.btn-dark');
+    bntDark.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+    })
 }
