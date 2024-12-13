66b44d71ae89a464fa16ccba5312e9554ec61c02
Damian Escobar <damianescobar2001@gmail.com>
2024-12-13 17:21:38 -0300

    A full con php

--- admin/index.php
+++ admin/index.php
@@ -8,6 +8,8 @@
         <h1>Administrador de Bienes Raices</h1>
 
         <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
+
+
     </main>
 
     <?php incluirTemplate('footer')?>
--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -1,6 +1,31 @@
 <?php 
-  require '../../includes/funciones.php';
+  //Base de datos
+  require '../../includes/config/database.php';
+  $db = conectarDB();
 
+  if($_SERVER["REQUEST_METHOD"] === "POST") {
+    /*
+    echo '<pre>';
+    var_dump($_POST);
+    echo '</pre>';
+    */
+
+    $titulo = $_POST['titulo'];
+    $precio = $_POST['precio'];
+    $descripcion = $_POST['descripcion'];
+    $habitaciones = $_POST['habitaciones'];
+    $wc = $_POST['wc'];
+    $estacionamiento = $_POST['estacionamientos'];
+    $vendedorId = $_POST['vendedor'];
+
+    //Insertar en la base de datos
+    $query = "INSERT INTO propiedades (titulo, precio, descripcion, habitaciones, wc, estacionamiento, vendedores_id) VALUES ('$titulo', '$precio', '$descripcion', '$habitaciones', '$wc', '$estacionamiento', '$vendedorId')";
+
+    $resultado = mysqli_query($db, $query);
+  }
+
+
+  require '../../includes/funciones.php';
   incluirTemplate('header'); 
 ?>
 
@@ -8,6 +33,87 @@
         <h1>Crear</h1>
 
         <a href="/admin" class="boton boton-verde">Volver</a>
+
+        <form class="formulario" action="/admin/propiedades/crear.php" method="POST">
+          <fieldset>
+            <legend>Información General</legend>
+
+            <label for="titulo">Titulo:</label>
+            <input 
+              type="text" 
+              id="titulo" 
+              name="titulo" 
+              placeholder="Titulo propiedad"
+            >
+
+            <label for="precio">Precio:</label>
+            <input 
+              type="number" 
+              id="precio" 
+              name="precio" 
+              placeholder="Titulo propiedad"
+            >
+
+            <label for="imagen">Imagen:</label>
+            <input 
+              type="file" 
+              id="imagen" 
+              accept="iamge/jpeg, image/png"
+            >
+
+            <label for="descripcion">Descripción:</label>
+            <textarea name="descripcion" id="descripcion"></textarea>
+
+          </fieldset>
+
+          <fieldset>
+            <legend>Información de la Propiedad</legend>
+
+            <label for="habitaciones">Habitaciones:</label>
+            <input 
+              type="number" 
+              id="habitaciones"
+              name="habitaciones" 
+              placeholder="Ej: 3" 
+              min="1" max="9"
+            >
+
+            <label for="wc">Baños:</label>
+            <input 
+              type="number" 
+              id="wc"
+              name="wc" 
+              placeholder="Ej: 3" 
+              min="1" max="9"
+            >
+            
+            <label for="estacionamientos">Estacionamientos:</label>
+            <input 
+              type="number" 
+              id="estacionamientos"
+              name="estacionamientos" 
+              placeholder="Ej: 2" 
+              min="1" max="9"
+            >
+
+          </fieldset>
+
+          <fieldset>
+            <legend>Vendedor</legend>
+
+            <select name="vendedor" id="">
+              <option value="1">Damian</option>
+              <option value="2">Dahiana</option>
+            </select>
+
+          </fieldset>
+
+          <input 
+            type="submit" 
+            value="Crear Propiedad" 
+            class="boton boton-verde"
+            >
+        </form>
     </main>
 
     <?php incluirTemplate('footer')?>
